cf53126f82247b7ff3737f6cde7b4e6ac5a8fb1c
Eriel Victor <erielvictor0@gmail.com>
2024-03-28 14:52:47 -0300

    Update questao 07.py

--- Python list 3/questao 07.py
+++ Python list 3/questao 07.py
@@ -7,3 +7,26 @@
 #entre 80 e 120(inclusive, <=) -> normal
 #entre 120 e 200(inclusive, <=) .> caro
 # maior que 200 -> caro
+
+preco = float(input("Informe o valor do produto: R$ "))
+
+if preco <= 50:
+    nv_preco = preco +(preco * 0.05)
+
+elif preco > 50 and preco <= 100:
+    nv_preco = preco + (preco * 0.10)
+
+elif preco > 100:
+    mv_preco = preco + (preco * 0.15)
+
+if nv_preco <= 80:
+    print(f"A classificação do novo preço é: Barato. Sendo o valor final igual a: R$ {nv_preco}")
+
+elif nv_preco > 80 and nv_preco <= 120:
+    print(f"A classificação do novo preço é: Normal. Sendo o valor final igual a: R$ {nv_preco}")
+
+elif nv_preco > 120 and nv_preco <= 200:
+    print(f"A classificação do novo preço é: Caro. Sendo o valor final igual a: R$ {nv_preco}")
+
+else:
+    print(f"A classificação do novo preço é: Muito caro. Sendo o valor final igual a: R$ {nv_preco}")
