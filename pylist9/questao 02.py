537c76aa0bd652fc018c4a1bc5f8e2a2e81baea5
Eriel Victor <erielvictor0@gmail.com>
2024-05-26 18:19:27 -0300

    Reviewed
    I've changed the second question, bc it was incorrect

--- pylist9/questao 02.py
+++ pylist9/questao 02.py
@@ -1,12 +1,38 @@
 # Faça um programa que leia o nome de 3 pessoas, 
 #cadastrando-as em um arquivo. 
 #Em seguida, o programa deverá exibir apenas o nome da 2 pessoa cadastra
-name = []
+
+arq = 'nomes.txt'
+dados = []
+
+def criarArq(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Houve um error')
+        nome = str(input("Dê um nome ao arquivo: "))
+        a = open(nome, 'wt+')
+        a.close()
+    else:
+        print('Arquivo Criado') 
+
 
 def cadastrar(nome):
+    arq = open('nomes.txt', 'a')
+    
     for i in range(3):
         nome = str(input("Informe o seu nome: "))
-        name.append(nome)
+        arq.write((nome) + '\n')
+    arq.close()
 
-cadastrar(name)
-print(name[:2])
+def mostrar(dados):
+    arq = open('nomes.txt', 'r')
+    dados = arq.readlines()
+    print(dados[1])
+    arq.close()
+
+
+criarArq(arq)
+cadastrar(arq)
+mostrar(dados)
